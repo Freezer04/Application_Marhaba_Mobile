8ce84c067fdbf6d162898cdcfef4cad23efc6ae5
Mohammed Kabli <76818640+mohammedkabli@users.noreply.github.com>
2022-05-25 15:47:10 +0100

    ♻️ refactory code

--- screens/home.js
+++ screens/home.js
@@ -1,60 +1,148 @@
-import React from 'react';
-import { ScrollView, StyleSheet, TextInput, Text } from 'react-native';
-import { View } from 'react-native-animatable';
+
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import Carousel from 'react-native-snap-carousel';
+import Feather from 'react-native-vector-icons/Feather';
+
+import BannerSlider from '../component/BandelSild';
+import {windowWidth} from '../component/Dimensions';
+
+import {freeGames, paidGames, sliderData} from '../component/data';
+import CustomSwitch from '../component/CustomSwitch';
+import ListItem from '../component/ListItems';
 import Header from '../component/header';
-import Cards from '../component/cards';
-import { FontAwesome } from '@expo/vector-icons';
 
+export default function MealsScreen({navigation}) {
+  const [gamesTab, setGamesTab] = useState(1);
+  const [Anoonce , setAnnonce] = useState([]);
+ 
+  const renderBanner = ({item, index}) => {
+    return <BannerSlider data={item} />;
+  };
 
-
-
-function HomeScreen () {
+  const onSelectSwitch = value => {
+    setGamesTab(value);
+  };
 
   return (
-    <ScrollView style={styles.container} >
+    <SafeAreaView  style={{flex: 1, backgroundColor: '#fff'}}>
       <Header/>
-      <View >
-        <Text style={{ fontSize: 20, marginLeft: 18, fontFamily: 'poppins-bold', marginTop: 5, color: "#bc8c4c" }}>Meals List</Text>
-      </View>
-      <View style={styles.search}>
-        <TextInput
-            style={styles.TextInput}
-            placeholder="Search"
-            placeholderTextColor="#C1C0BB"
+      <ScrollView style={{padding: 20}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            marginBottom: 20,
+          }}>
+          <Text style={{fontSize: 18, fontFamily: 'poppins-regular'}}>
+            Hello John Doe
+          </Text>
+          <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
+            <ImageBackground
+              source={require('../assets/profile.png')}
+              style={{width: 35, height: 35}}
+              imageStyle={{borderRadius: 25}}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            borderColor: '#C6C6C6',
+            borderWidth: 1,
+            borderRadius: 8,
+            paddingHorizontal: 10,
+            paddingVertical: 8,
+          }}>
+          <Feather
+            name="search"
+            size={20}
+            color="#C6C6C6"
+            style={{marginRight: 5}}
           />
-      
-      <FontAwesome name="search" size={30} color="#F2C978" />
-      </View>
-      <Cards/>
-     
-    </ScrollView>
-)
+          <TextInput placeholder="Search" />
+        </View>
+
+        <View
+          style={{
+            marginVertical: 15,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}>
+          <Text style={{fontSize: 18, fontFamily: 'poppins-regular'}}>
+          Popular Announcement
+          </Text>
+          <TouchableOpacity onPress={() => {}}>
+            <Text style={{color: '#F2C978'}}>See all</Text>
+          </TouchableOpacity>
+        </View>
+
+        <Carousel
+          // ref={c => {
+          //   this._carousel = c;
+          // }}
+          data={sliderData}
+          renderItem={renderBanner}
+          sliderWidth={windowWidth - 40}
+          itemWidth={300}
+          loop={true}
+        />
+
+        <View style={{marginVertical: 20}}>
+          <CustomSwitch
+            selectionMode={1}
+            option1="Free to order"
+            option2="Order Now"
+            onSelectSwitch={onSelectSwitch}
+          />
+        </View>
+
+        {gamesTab == 1 &&
+          freeGames.map(item => (
+            <ListItem
+              key={item.id}
+              photo={item.poster}
+              title={item.title}
+              subTitle={item.subtitle}
+              isFree={item.isFree}
+              onPress={() =>
+                navigation.navigate('Home', {
+                  title: item.title,
+                  id: item.id,
+                })
+              }
+            />
+          ))}
+        {gamesTab == 2 &&
+          paidGames.map(item => (
+            <ListItem
+              key={item.id}
+              photo={item.poster}
+              title={item.title}
+              subTitle={item.subtitle}
+              isFree={item.isFree}
+              price={item.price}
+              onPress={() =>
+                navigation.navigate('Orders', {
+                  title: item.title,
+                  id: item.id,
+                })
+              }
+            />
+          ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
- 
-const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-    backgroundColor: "#EBE9E6",
-  },
-  search:{
-    display:'flex',
-    flexDirection: "row",
-    alignItems: 'center',
-    justifyContent:'space-around',
-    marginRight: 15,
-    paddingVertical:12,
-    
-  },
-  TextInput: {
-    height: 40,
-    width:"80%",
-    padding: 10,
-    marginLeft: 10,
-    backgroundColor: "#ffff",
-    borderRadius: 5,
-    fontFamily:'poppins-regular'
 
-  }
-});
 
-export default HomeScreen;
+
